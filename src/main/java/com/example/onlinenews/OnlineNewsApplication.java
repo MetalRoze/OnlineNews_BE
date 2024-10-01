22483ea20ed9bd610e39a7d886f89d4871359405
wlstj <22020306jinseoyeon@gmail.com>
2024-10-01 19:43:40 +0900

    init: 환경설정

---
+++ src/main/java/com/example/onlinenews/OnlineNewsApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinenews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineNewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onlinenews/OnlineNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinenews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineNewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

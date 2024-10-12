9fe87163e1cc811c21f1393cb9955dc5ee689eea
daxxn <dahyeonhan4529@gmail.com>
2024-10-13 04:03:25 +0900

    chore : Getter 어노테이션 추가

--- src/main/java/com/example/onlinenews/article/entity/State.java
+++ src/main/java/com/example/onlinenews/article/entity/State.java
@@ -1,10 +1,14 @@
 package com.example.onlinenews.article.entity;
 
-public enum State {
+import lombok.Getter;
+
+@Getter
+public enum State{
     REQUESTED("승인 요청된 상태"),
     HOLDING("승인 보류된 상태"),
     APPROVED("기사 승인된 상태");
 
+    // 한글 설명을 리턴하는 메소드
     private final String description;
 
     // 생성자
@@ -12,8 +16,4 @@ public enum State {
         this.description = description;
     }
 
-    // 한글 설명을 리턴하는 메소드
-    public String getDescription() {
-        return description;
-    }
 }

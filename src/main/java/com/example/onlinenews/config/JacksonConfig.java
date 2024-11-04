77cc4a68e3649b28f10fc66cf01270b2f4c8f1d5
daxxn <dahyeonhan4529@gmail.com>
2024-11-04 15:14:05 +0900

    fix(config): LocalDateTime을 위해 JacksonConfig에 jodaModule 추가

--- src/main/java/com/example/onlinenews/config/JacksonConfig.java
+++ src/main/java/com/example/onlinenews/config/JacksonConfig.java
@@ -1,11 +1,11 @@
 package com.example.onlinenews.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 public class JacksonConfig {
@@ -13,6 +13,7 @@ public class JacksonConfig {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JodaModule()); // JodaModule 등록 추가
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         return objectMapper;

d549e783c6fd6943d9d81793f33001e417e04792
eunnnju <sbsdy3@gmail.com>
2024-11-06 15:48:32 +0900

    feat : 신문사 타입 변경

--- src/main/java/com/example/onlinenews/publisher/dto/PublisherCreateRequestDTO.java
+++ src/main/java/com/example/onlinenews/publisher/dto/PublisherCreateRequestDTO.java
@@ -1,5 +1,6 @@
 package com.example.onlinenews.publisher.dto;
 
+import com.example.onlinenews.publisher.entity.Type;
 import jakarta.persistence.Column;
 import lombok.Data;
 
@@ -8,5 +9,5 @@ public class PublisherCreateRequestDTO {
     private String publisher_name;
     private String publisher_url;
     private String publisher_img;
-    private String publisher_type;
+    private Type publisher_type;
 }

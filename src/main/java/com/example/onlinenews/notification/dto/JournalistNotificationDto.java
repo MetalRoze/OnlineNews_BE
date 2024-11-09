6c2703f3db507fa6303f2f809366ddcafaf50c32
wlstj <22020306jinseoyeon@gmail.com>
2024-11-09 23:03:13 +0900

    chore: 기자 dto sentBy추가

--- src/main/java/com/example/onlinenews/notification/dto/JournalistNotificationDto.java
+++ src/main/java/com/example/onlinenews/notification/dto/JournalistNotificationDto.java
@@ -1,6 +1,7 @@
 package com.example.onlinenews.notification.dto;
 
 import com.example.onlinenews.notification.entity.JournalistNotification;
+import com.example.onlinenews.notification.entity.NotificationType;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -11,16 +12,24 @@ public class JournalistNotificationDto {
     private String notificationContent;
     private String comment;
     private LocalDateTime createdAt;
+    private String sentBy;
 
-    public JournalistNotificationDto(Long id, String notificationContent, String comment, LocalDateTime createdAt) {
+    public JournalistNotificationDto(Long id, String sentBy, String notificationContent, String comment, LocalDateTime createdAt) {
         this.id = id;
+        this.sentBy = sentBy;
         this.notificationContent = notificationContent;
         this.comment = comment;
         this.createdAt = createdAt;
     }
     public static JournalistNotificationDto fromEntity(JournalistNotification notification){
+        NotificationType notificationType = notification.getType();
+        String sentBy="";
+        if(notificationType.getMessage().equals("REPORTER_LIKE")){
+            sentBy= notification.getArticleLike().getUser().getName();
+        }
         return new JournalistNotificationDto(
                 notification.getId(),
+                sentBy,
                 notification.getRequest().getArticle().getTitle() + " " + notification.getType().getMessage(),
                 notification.getRequest().getComment(),
                 notification.getCreatedAt()

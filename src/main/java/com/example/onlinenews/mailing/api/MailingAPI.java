77342856240854e0ac4665287080fb60e04a2712
daxxn <dahyeonhan4529@gmail.com>
2024-11-22 19:21:01 +0900

    feat(mailing): 메일링 수신 여부 설정 API 생성

--- src/main/java/com/example/onlinenews/mailing/api/MailingAPI.java
+++ src/main/java/com/example/onlinenews/mailing/api/MailingAPI.java
@@ -1,8 +1,11 @@
 package com.example.onlinenews.mailing.api;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,4 +19,13 @@ public interface MailingAPI {
 
     @GetMapping("/sendDailyTest")
     ResponseEntity<?> sendDailyMailTest(@RequestParam String to);
+
+    @PostMapping("/subscribe")
+    @Operation(summary = "메일링 수신 설정")
+    ResponseEntity<?> subscribeMailing(@RequestParam Long userId);
+
+    @DeleteMapping("/unsubscribe")
+    @Operation(summary = "메일링 수신 해지")
+    ResponseEntity<?> unsubscribeMailing(@RequestParam Long userId);
+
 }

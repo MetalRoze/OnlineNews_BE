3df5067d38f7322e577a02daec218823f9db1c1f
eunnnju <sbsdy3@gmail.com>
2024-11-26 14:52:52 +0900

    fix : 출판사 조회 api 수정

--- src/main/java/com/example/onlinenews/publisher/service/PublisherService.java
+++ src/main/java/com/example/onlinenews/publisher/service/PublisherService.java
@@ -35,12 +35,14 @@ public class PublisherService {
 
     public List<PublisherDto> list(){
         return publisherRepository.findAll().stream()
+                .filter(publisher -> publisher.getId() != 19) // publisherId가 19인 건 제외
                 .map(PublisherDto::fromEntity)
                 .collect(Collectors.toList());
     }
 
     public List<PublisherDto> getByTypeList(String pub_type){
         return publisherRepository.findByType(pub_type).stream()
+                .filter(publisher -> publisher.getId() != 19) // publisherId가 19인 건 제외
                 .map(PublisherDto::fromEntity)
                 .collect(Collectors.toList());
     }

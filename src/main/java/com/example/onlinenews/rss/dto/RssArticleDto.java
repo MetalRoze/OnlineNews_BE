ed0d44c2cf53218e45e277162b3a9ac530a0dc43
wlstj <22020306jinseoyeon@gmail.com>
2024-11-26 16:51:50 +0900

    refact: Rss dto 수정

--- src/main/java/com/example/onlinenews/rss/dto/RssArticleDto.java
+++ src/main/java/com/example/onlinenews/rss/dto/RssArticleDto.java
@@ -5,24 +5,23 @@ import lombok.Data;
 
 @Data
 public class RssArticleDto {
-    private Long pubId;
+    private Long publisherId;
     private String publisherName;
     private String title;
     private String subtitle;
+    private String content;
     private String url;
-    private String author;
+    private String userName;
     private String createdAt;
-    private String imgUrl;
 
     @Builder
-    public RssArticleDto(Long pubId, String publisherName, String title, String subtitle, String url, String author, String createdAt, String imgUrl) {
-        this.pubId=pubId;
+    public RssArticleDto(Long publisherId, String publisherName, String title, String subtitle, String url, String userName, String createdAt, String imgUrl) {
+        this.publisherId=publisherId;
         this.publisherName = publisherName;
         this.title = title;
         this.subtitle = subtitle;
         this.url = url;
-        this.author = author;
+        this.userName = userName;
         this.createdAt=createdAt;
-        this.imgUrl=imgUrl;
     }
 }

67e07025932393486806cc19f37d42b83738063f
eunnnju <sbsdy3@gmail.com>
2024-12-08 23:59:06 +0900

    fix : 오류 수정

--- src/main/java/com/example/onlinenews/subscription/repository/SubscriptionRepository.java
+++ src/main/java/com/example/onlinenews/subscription/repository/SubscriptionRepository.java
@@ -7,10 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
     List<Subscription> findByUserId(Long userId);
+    Optional<Subscription> findByPublisherIdAndUser(Long publisherId, User user);
     boolean existsByUserAndPublisher(User user,Publisher publisher);
 
 }
